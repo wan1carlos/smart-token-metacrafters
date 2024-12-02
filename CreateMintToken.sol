d6d38f8a8babf0c15ad6cb4dd886a81bec5ede1b
Jc Francisco <84316334+wan1carlos@users.noreply.github.com>
2024-12-02 13:31:02 +0800

    Update CreateMintToken.sol

--- CreateMintToken.sol
+++ CreateMintToken.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    construct() ERC20("WeiWei", "Wei) Ownable(msg.sender) {
-        //Deployer is the owner
-}
- // Minting function that is only for the owne
+     // Constructor for the token
+    constructor() ERC20("WeiWei", "WEI") Ownable(msg.sender) {
+        // Deployer is the owner
+    }
+    // Minting function that is only for the owne
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-
     // Any user can burn
     function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
+        _burn(_msgSender(), amount);
+    }
+    //Funtion to transfer, also checks if there is sufficient balance
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(balanceOf(_msgSender()) >= amount, "Transfer failed: there is insufficient balance");
+        return super.transfer(recipient, amount);
     }
 }
